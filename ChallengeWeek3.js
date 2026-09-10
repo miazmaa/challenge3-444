699bb0a0cd90da85e69bb35ece2fb51ca7fcc1fc
miazmaa <alc032731@gmail.com>
2026-09-10 14:01:49 +0000

    changed camera position and parameters to match the instructions

--- ChallengeWeek3.js
+++ ChallengeWeek3.js
@@ -23,8 +23,8 @@ window.addEventListener("keyup", (event) => {
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x87ceeb);
 
-const camera = new THREE.PerspectiveCamera(60, 1, 0.1, 100);
-camera.position.set(0, 8, 16);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+camera.position.set(0, 5, 15);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
